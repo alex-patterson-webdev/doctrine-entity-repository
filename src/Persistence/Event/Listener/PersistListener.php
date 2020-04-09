6b6b963aa3abefecf23045f63d4f18c541ff4586
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-04-09 19:23:48 +0000

    Deleted factory classes as these are currently unused and added logger to the PersistListener.

--- src/Persistence/Event/Listener/PersistListener.php
+++ src/Persistence/Event/Listener/PersistListener.php
@@ -8,6 +8,7 @@ use Arp\DoctrineEntityRepository\Constant\PersistEventOption;
 use Arp\DoctrineEntityRepository\Constant\PersistMode;
 use Arp\DoctrineEntityRepository\Persistence\Event\PersistEvent;
 use Arp\DoctrineEntityRepository\Persistence\Exception\PersistServiceException;
+use Psr\Log\LoggerInterface;
 
 /**
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
@@ -16,22 +17,49 @@ use Arp\DoctrineEntityRepository\Persistence\Exception\PersistServiceException;
 final class PersistListener
 {
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Register the entity for persistence.
+     *
      * @param PersistEvent $event
      *
-     * @throws PersistServiceException
+     * @throws PersistServiceException If the entity cannot be found or persisted.
      */
     public function __invoke(PersistEvent $event): void
     {
-        if ($event->hasEntity()) {
-            return;
+        $entity = $event->getEntity();
+
+        if (null === $entity) {
+            $errorMessage = 'Unable to perform entity persist operation : The entity is undefined';
+
+            $this->logger->error($errorMessage, compact('event'));
+
+            throw new PersistServiceException($errorMessage);
         }
 
         $persistMode = $event->getParameters()->getParam(PersistEventOption::PERSIST_MODE, PersistMode::ENABLED);
 
         if (PersistMode::ENABLED !== $persistMode) {
+            $this->logger->info(
+                sprintf('Skipping entity persist operation with persist mode \'%s\'', $persistMode),
+                compact('entity', 'persistMode')
+            );
             return;
         }
 
-        $event->getPersistService()->persist($event->getEntity());
+        $this->logger->info('Performing entity persist operations', compact('entity'));
+
+        $event->getPersistService()->persist($entity);
     }
 }
