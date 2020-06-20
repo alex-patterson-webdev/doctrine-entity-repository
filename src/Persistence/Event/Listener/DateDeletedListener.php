471f64d656ae07a515b24ae3f30d2fb6b4fe09d9
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-06-20 09:12:07 +0000

    Added new DateDeletedListener that shares the same setup and test cases as the existing DateUpdateListner and DateCreatedListner

--- src/Persistence/Event/Listener/DateDeletedListener.php
+++ src/Persistence/Event/Listener/DateDeletedListener.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Arp\DoctrineEntityRepository\Persistence\Event\Listener;
 
-use Arp\DateTime\Exception\DateTimeFactoryException;
-use Arp\DoctrineEntityRepository\Constant\DeleteMode;
+use Arp\DoctrineEntityRepository\Constant\DateDeleteMode;
 use Arp\DoctrineEntityRepository\Constant\EntityEventOption;
 use Arp\DoctrineEntityRepository\Persistence\Event\EntityEvent;
+use Arp\DoctrineEntityRepository\Persistence\Exception\PersistenceException;
 use Arp\Entity\DateDeletedAwareInterface;
 
 /**
@@ -17,29 +17,55 @@ use Arp\Entity\DateDeletedAwareInterface;
 class DateDeletedListener extends AbstractDateTimeListener
 {
     /**
+     * Check if the created entity implements DateCreatedAwareInterface and set a new date time instance on the
+     * dateCreated property.
+     *
      * @param EntityEvent $event
      *
-     * @throws DateTimeFactoryException
+     * @throws PersistenceException
      */
-    public function __invoke(EntityEvent $event)
+    public function __invoke(EntityEvent $event): void
     {
-        $deleteMode = $event->getParameters()->getParam(EntityEventOption::DELETE_MODE, DeleteMode::HARD);
+        $entityName = $event->getEntityName();
         $entity = $event->getEntity();
 
-        if (
-            null === $entity
-            || (! $entity instanceof DateDeletedAwareInterface)
-            || DeleteMode::HARD === $deleteMode
-        ) {
+        if (null === $entity || !$entity instanceof DateDeletedAwareInterface) {
+            $this->logger->debug(
+                sprintf(
+                    'Ignoring update date time for entity \'%s\': The entity does not implement \'%s\'',
+                    $entityName,
+                    DateDeletedAwareInterface::class
+                )
+            );
             return;
         }
 
-        $dateDeleted = $this->dateTimeFactory->createDateTime();
+        $mode = $event->getParameters()->getParam(EntityEventOption::DATE_DELETED_MODE, DateDeleteMode::ENABLED);
+        $entityId = $entity->getId();
+
+        if (DateDeleteMode::ENABLED !== $mode) {
+            $this->logger->info(
+                sprintf(
+                    'The date time update of field \'dateDeleted\' '
+                    . 'has been disabled for entity \'%s::%s\' using configuration option \'%s\'',
+                    $entityName,
+                    $entityId,
+                    EntityEventOption::DATE_DELETED_MODE
+                )
+            );
+            return;
+        }
+
+        $dateDeleted = $this->createDateTime($entityName, $entityId);
         $entity->setDateDeleted($dateDeleted);
 
         $this->logger->info(
-            'The \'dateDeleted\' property was set to the current date time',
-            compact('entity', 'dateDeleted')
+            sprintf(
+                'The \'dateDeleted\' property for entity \'%s::%s\' has been updated with new date time \'%s\'',
+                $entityName,
+                $entityId,
+                $dateDeleted->format(\DateTime::ATOM)
+            )
         );
     }
 }
