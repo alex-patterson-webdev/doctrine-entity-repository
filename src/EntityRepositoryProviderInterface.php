4b5be0fa3bb8a38277fee68d30836226fc8f81e6
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-06-27 12:06:45 +0000

    Modified the existing EntityRepoositoryProviderInterface to use hasRepository() and getRepository() methods

--- src/EntityRepositoryProviderInterface.php
+++ src/EntityRepositoryProviderInterface.php
@@ -4,18 +4,28 @@ declare(strict_types=1);
 
 namespace Arp\DoctrineEntityRepository;
 
-use Psr\Container\ContainerInterface;
+use Arp\DoctrineEntityRepository\Exception\EntityRepositoryNotFoundException;
 
 /**
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\DoctrineEntityRepository
  */
-interface EntityRepositoryProviderInterface extends ContainerInterface
+interface EntityRepositoryProviderInterface
 {
     /**
      * @param string $entityName
      *
-     * @return EntityRepositoryInterface|null
+     * @return bool
      */
-    public function getEntityRepository(string $entityName): ?EntityRepositoryInterface;
+    public function hasRepository(string $entityName): bool;
+
+    /**
+     * @param string $entityName
+     * @param array  $options
+     *
+     * @return EntityRepositoryInterface
+     *
+     * @throws EntityRepositoryNotFoundException
+     */
+    public function getRepository(string $entityName, array $options = []): EntityRepositoryInterface;
 }
