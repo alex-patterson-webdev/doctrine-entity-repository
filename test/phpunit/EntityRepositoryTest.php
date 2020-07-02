95ffab290805fb0ba65ac8af4522f36e917628cc
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-07-02 14:32:20 +0000

    Added new unit test cases for the EntityRepository

--- test/phpunit/EntityRepositoryTest.php
+++ test/phpunit/EntityRepositoryTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ArpTest\DoctrineEntityRepository;
 
+use Arp\DoctrineEntityRepository\Constant\EntityEventOption;
+use Arp\DoctrineEntityRepository\Constant\QueryServiceOption;
 use Arp\DoctrineEntityRepository\EntityRepository;
 use Arp\DoctrineEntityRepository\EntityRepositoryInterface;
 use Arp\DoctrineEntityRepository\Exception\EntityRepositoryException;
@@ -125,7 +127,7 @@ class EntityRepositoryTest extends TestCase
 
         $entityId = 'FOO123';
 
-        $exceptionMessage = 'This is a test exception message';
+        $exceptionMessage = 'This is a test exception message for find()';
         $exception = new \Exception($exceptionMessage);
 
         $this->queryService->expects($this->once())
@@ -197,5 +199,380 @@ class EntityRepositoryTest extends TestCase
             ->willReturn($entity);
 
         $this->assertSame($entity, $repository->find($entityId));
+    }
+
+    /**
+     * Assert that if the findOneBy() will catch and rethrow exception messages as EntityRepositoryException.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::findOneBy
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testFindOneByWillThrowEntityRepositoryExceptionIfTheQueryCannotBePerformed(): void
+    {
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        $criteria = [
+            'name'  => 'Test',
+            'hello' => 'World',
+            'test'  => 123,
+        ];
+
+        $exceptionMessage = 'This is a test exception message for findOneById()';
+        $exceptionCode = 456;
+
+        $exception = new \Exception($exceptionMessage, $exceptionCode);
+
+        $this->queryService->expects($this->once())
+            ->method('findOne')
+            ->with($criteria)
+            ->willThrowException($exception);
+
+        $errorMessage = sprintf('Unable to find entity of type \'%s\': %s', $this->entityName, $exceptionMessage);
+
+        $this->logger->error($errorMessage, compact('exception', 'criteria'));
+
+        $this->expectException(EntityRepositoryException::class);
+        $this->expectExceptionMessage($errorMessage);
+        $this->expectExceptionCode($exceptionCode);
+
+        $repository->findOneBy($criteria);
+    }
+
+    /**
+     * Assert that findOneBy() will return NULL if the provided criteria doesn't match any existing entities.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::findOneBy
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testFindOneByWillReturnNullIfAMatchingEntityCannotBeFound(): void
+    {
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        $criteria = [
+            'foo'  => 'bar',
+            'test' => 789,
+        ];
+
+        $this->queryService->expects($this->once())
+            ->method('findOne')
+            ->with($criteria)
+            ->willReturn(null);
+
+        $this->assertNull($repository->findOneBy($criteria));
+    }
+
+    /**
+     * Assert findOneBy() will return a single matched entity instance.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::findOneBy
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testFindOneByWillReturnASingleMatchEntity(): void
+    {
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        $criteria = [
+            'fred' => 'test',
+            'bob'  => 123,
+        ];
+
+        /** @var EntityInterface|MockObject $entity */
+        $entity = $this->getMockForAbstractClass(EntityInterface::class);
+
+        $this->queryService->expects($this->once())
+            ->method('findOne')
+            ->with($criteria)
+            ->willReturn($entity);
+
+        $this->assertSame($entity, $repository->findOneBy($criteria));
+    }
+
+    /**
+     * Assert that calls to findAll() will proxy to findBy() with an empty array.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::findAll
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testFindAllWillProxyAnEmptyArrayToFindBy(): void
+    {
+        /** @var EntityRepository|MockObject $repository */
+        $repository = $this->getMockBuilder(EntityRepository::class)
+            ->setConstructorArgs(
+                [
+                    $this->entityName,
+                    $this->queryService,
+                    $this->persistService,
+                    $this->logger,
+                ]
+            )
+            ->onlyMethods(['findBy'])
+            ->getMock();
+
+        /** @var EntityInterface[]|MockObject[] $entities */
+        $entities = [
+            $this->getMockForAbstractClass(EntityInterface::class),
+            $this->getMockForAbstractClass(EntityInterface::class),
+            $this->getMockForAbstractClass(EntityInterface::class),
+        ];
+
+        $repository->expects($this->once())
+            ->method('findBy')
+            ->with([])
+            ->willReturn($entities);
+
+        $this->assertSame($entities, $repository->findAll());
+    }
+
+    /**
+     * Assert that the findBy() method will catch \Throwable exceptions, log the exception data and rethrow as a
+     * EntityRepositoryException.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::findBy
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testFindByWillCatchAndRethrowEntityRepositoryExceptionOnFailure(): void
+    {
+        /** @var EntityRepository|MockObject $repository */
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        $criteria = [];
+        $options = [];
+
+        $exceptionMessage = 'This is a foo test exception';
+        $exceptionCode = 456;
+        $exception = new \Error($exceptionMessage, $exceptionCode);
+
+        $this->queryService->expects($this->once())
+            ->method('findMany')
+            ->with($criteria, $options)
+            ->willThrowException($exception);
+
+        $errorMessage = sprintf(
+            'Unable to return a collection of type \'%s\': %s',
+            $this->entityName,
+            $exceptionMessage
+        );
+
+        $this->logger->expects($this->once())
+            ->method('error')
+            ->with($errorMessage, compact('exception', 'criteria', 'options'));
+
+        $this->expectException(EntityRepositoryException::class);
+        $this->expectExceptionMessage($errorMessage);
+
+        $repository->findBy([]);
+    }
+
+    /**
+     * Assert the required search arguments are passed to findMany().
+     *
+     * @param array $data
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::findBy
+     *
+     * @dataProvider getFindByWithSearchArgumentsData
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testFindByWillPassValidSearchArguments(array $data): void
+    {
+        /** @var EntityRepository|MockObject $repository */
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        $criteria = $data['criteria'] ?? [];
+        $options = [];
+
+        if (isset($data['order_by'])) {
+            $options[QueryServiceOption::ORDER_BY] = $data['order_by'];
+        }
+
+        if (isset($data['limit'])) {
+            $options[QueryServiceOption::LIMIT] = $data['limit'];
+        }
+
+        if (isset($data['offset'])) {
+            $options[QueryServiceOption::OFFSET] = $data['offset'];
+        }
+
+        /** @var EntityInterface[]|MockObject[] $collection */
+        $collection = [
+            $this->getMockForAbstractClass(EntityInterface::class),
+            $this->getMockForAbstractClass(EntityInterface::class),
+            $this->getMockForAbstractClass(EntityInterface::class),
+        ];
+
+        $this->queryService->expects($this->once())
+            ->method('findMany')
+            ->with($criteria, $options)
+            ->willReturn($collection);
+
+        $result = $repository->findBy(
+            $criteria,
+            $data['order_by'] ?? null,
+            $data['limit'] ?? null,
+            $data['offset'] ?? null
+        );
+
+        $this->assertSame($collection, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function getFindByWithSearchArgumentsData(): array
+    {
+        return [
+            [
+                // Empty Data
+                [
+
+                ],
+            ],
+
+            [
+                [
+                    'criteria' => [
+                        'name' => 'test',
+                        'hello' => 'foo',
+                    ],
+                ]
+            ],
+
+            [
+                [
+                    'limit' => 100,
+                ]
+            ],
+
+            [
+                [
+                    'offset' => 10,
+                ]
+            ],
+
+            [
+                [
+                    'order_by' => [
+                        'name' => 'desc',
+                        'foo' => 'asc',
+                    ],
+                ]
+            ],
+
+            [
+                [
+                    'criteria' => [
+                        'name' => 'test',
+                        'hello' => 'foo',
+                        'foo' => 123
+                    ],
+                    'offset' => 7,
+                    'limit' => 1000,
+                    'order_by' => [
+                        'hello' => 'asc',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Assert that errors during save that throw a \Throwable exception are caught, logged and rethrown as a
+     * EntityRepositoryException.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::save
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testSaveWillCatchThrowableAndRethrowEntityRepositoryException(): void
+    {
+        /** @var EntityRepository|MockObject $repository */
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        /** @var EntityInterface|MockObject $entity */
+        $entity = $this->getMockForAbstractClass(EntityInterface::class);
+
+        $exceptionMessage = 'This is a test exception message for save()';
+        $exceptionCode = 999;
+        $exception = new \Error($exceptionMessage, $exceptionCode);
+
+        $errorMessage = sprintf('Unable to save entity of type \'%s\': %s', $this->entityName, $exceptionMessage);
+
+        $this->persistService->expects($this->once())
+            ->method('save')
+            ->with($entity)
+            ->willThrowException($exception);
+
+        $this->expectException(EntityRepositoryException::class);
+        $this->expectExceptionMessage($errorMessage);
+        $this->expectExceptionCode($exceptionCode);
+
+        $this->assertSame($entity, $repository->save($entity));
+    }
+
+    /**
+     * Assert that calls to save() will result in the entity being passed to the persist service.
+     *
+     * @covers \Arp\DoctrineEntityRepository\EntityRepository::save
+     *
+     * @throws EntityRepositoryException
+     */
+    public function testSaveAndReturnEntityWithProvidedOptions(): void
+    {
+        $options = [
+            'foo' => 'bar',
+        ];
+
+        $repository = new EntityRepository(
+            $this->entityName,
+            $this->queryService,
+            $this->persistService,
+            $this->logger
+        );
+
+        /** @var EntityInterface|MockObject $entity */
+        $entity = $this->getMockForAbstractClass(EntityInterface::class);
+
+        $this->persistService->expects($this->once())
+            ->method('save')
+            ->with($entity, $options)
+            ->willReturn($entity);
+
+        $this->assertSame($entity, $repository->save($entity, $options));
     }
 }
